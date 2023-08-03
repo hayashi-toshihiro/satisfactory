0f280cf535e293080210cdf54bb517a17a83c62a
H.T <hayashi19980203@gmail.com>
2023-08-03 15:10:43 +0900

    rootの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  root 'recipes#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post 'recipes/card_update', to: 'recipes#card_update'
   post 'recipes/form_add', to: 'recipes#form_add'
