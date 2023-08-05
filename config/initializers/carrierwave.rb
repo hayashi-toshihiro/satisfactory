b4424112a907c788049d613d703d179e2a5cbf82
H.T <hayashi19980203@gmail.com>
2023-08-05 15:06:01 +0900

    carrierwave編集

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,16 +2,17 @@ require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
-CarrierWave.configure do |config|
-  config.storage :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_directory  = 'satisfactorycreate'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV['AKIAWOEBS25OXJLE4SMV'],
-    aws_secret_access_key: ENV['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'],
-    region: ENV['ap-northeast-1'],
-    path_style: true
-  }
-
+unless Rails.env.development? || Rails.env.test?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: 'AKIAWOEBS25OXJLE4SMV',
+      aws_secret_access_key: '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',
+      region: 'ap-northeast-1',
+      path_style: true
+    }
+  
+    config.fog_directory = 'satisfactorycreate'
+    config.cache_storage = :fog
+  end
 end
